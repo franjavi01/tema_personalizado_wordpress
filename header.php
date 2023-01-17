2d4f8502868f917971317e1956f2063c2e50a7d3
franjavi01 <fjsg0001@gmail.com>
2023-01-17 18:46:20 +0100

    ajustando menu y submenu

--- header.php
+++ header.php
@@ -10,17 +10,16 @@
 
 <body id="body">
     <header>
-        <nav class="header-menu navbar bg-primary navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <!-- <nav class="header-menu navbar bg-primary navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
 
 
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+    </a>
 
                 
-                    <?php wp_nav_menu(array('menu' => 'Menú principal')); ?> 
+                     <?php wp_nav_menu(array('menu' => 'Menú principal')); ?>
                 
                     
                 
@@ -32,6 +31,41 @@
 
             </div>
 
-        </nav>
+        </nav> -->
+
+        <nav class="header-menu navbar navbar-expand-lg navbar-dark bg-primary">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#opciones">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="opciones">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="index.php">
+                            <img src="#" alt="Logo" width="30px" height="30px" class="d-inline-block align-text-center">
+                        </a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
+                        </form>
+                    </ul>
+                </li>
+                <?php wp_nav_menu(array('menu' => 'Menú principal')); ?>
+                <!-- <li class="nav-item">
+                    <?php include 'fragmentos/noche.php' ?>
+                </li> -->
+            </ul>
+
+        </div>
+
+    </nav>
     </header>
     <main>
